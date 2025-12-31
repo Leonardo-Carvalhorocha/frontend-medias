55ac49fc179ebf9df3fe374d83dcd9b0620e22a5
Leonardo <leonardo2001darocha@gmail.com>
2025-12-30 22:44:27 -0300

    feature: adicionando no footer o versionamento

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,13 @@
 function Footer() {
   return (
-    <footer className="w-full py-4 text-center text-sm text-gray-600 border-t mt-8">
-      <span>
-        Desenvolvido por Leonardo Carvalho juntamente com Alexia Sarah Baltazar.
-      </span>
+    // <footer className="w-full py-4 text-center text-sm text-gray-600 border-t mt-8">
+    //   <span>
+    //     Desenvolvido por Leonardo Carvalho juntamente com Alexia Sarah Baltazar. Versão 1.0.0 
+    //   </span>
+    // </footer>
+    <footer className="w-full py-4 mt-8 border-t text-center text-sm text-gray-600">
+      Desenvolvido por Leonardo Carvalho juntamente com Alexia Sarah Baltazar.{' '}
+      Versão {__APP_VERSION__}
     </footer>
   );
 }
